022fb82218790348621164aa3ee6f2c1d53f292c
Andreas <rsouflaki@yahoo.com>
2013-08-18 08:37:06 +0200

    Add Rails example app

---
+++ resumeapp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Resumeapp::Application.config.secret_key_base = '025644324ff2dcd72b40e593b11639624616c09f2794184d016ee320b13bc3cb44970b985e12b79b668088ae93209643225219a4db616486c506526ec12d34c6'
